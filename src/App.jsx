e5c8aecf47f82237e4242c0a885328c7d6457e11
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-03-30 23:32:41 +0600

    profile info done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import CafeDetails from './component/CafeDetails/CafeDetails'
 import Header from './component/Header/Header'
 
+
+
 function App() {
  
 return (
--- src/component/Profile/Profile.jsx
+++ src/component/Profile/Profile.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Profile.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 const Profile = ({profile}) => {
+    const {Author_name,Blog_title,Publish_Date,Read_time,author_pic,cover_pic,id} = profile
     console.log(profile)
     return (
-        <div>
+        <div className='profile-container'>
+            <img className='cover-pic' src={cover_pic} alt="" />
+            
+            <div className="author-info">
+
+                <div className="autho-bio">
+                    <img className='author-pic'  src={author_pic} alt="" />
+                    <p className='name-date'>{Author_name} <br />{Publish_Date}</p>
+                </div>
+           
+                <div className="book-mark">
+                    <p>{Read_time}</p>
+                    <FontAwesomeIcon icon={faBookmark} />
+                </div>
+            
+            </div>
             
         </div>
     );
